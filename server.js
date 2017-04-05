720b89c53162d556536c48e4e28dc4f6e4dad7e5
Alexis Tyler <xo@wvvw.me>
2017-04-05 13:45:02 +0930

    add yarn file, update deps, switch to ES6

--- server.js
+++ server.js
@@ -1,57 +1,26 @@
 #!/usr/bin/env node
 
-// server.js
-//------------------------------
-//
-// 2013-08-18, Jonas Colmsjö
-//
-// Copyright Gizur AB 2013
-//
-// Simple DNS server based on dnsd developed by iriscouch
-//
-// Using Google JavaScript Style Guide - http://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml
-//
-//------------------------------
+import dnsd from 'dnsd';
+import nconf from 'nconf';
 
-var dnsd       = require('dnsd'),
-    nconf      = require('nconf');
+nconf.use('file', {
+    file: path.join(__dirname, 'redis-dns-config.json')
+});
+nconf.load();
 
+this.dnsInterface = nconf.get('dns_interface');
+this.dnsPort      = nconf.get('dns_port');
+this.dnsZone      = nconf.get('dns_zone');
+this.redisHost    = nconf.get('redis_host');
+this.redisPort    = nconf.get('redis_port');
 
-  nconf.use('file', { file: __dirname + '/redis-dns-config.json' });
-  nconf.load();
-
-  this.dnsInterface = nconf.get('dns_interface');
-  this.dnsPort      = nconf.get('dns_port');
-  this.dnsZone      = nconf.get('dns_zone');
-  this.redisHost    = nconf.get('redis_host');
-  this.redisPort    = nconf.get('redis_port');
-
-  // set logging level
-/*  switch(nconf.get('logging')) {
-
-    case 'debug':
-      helpers.logging_threshold  = helpers.logging.debug;
-      break;
-
-    case 'warning':
-      helpers.logging_threshold  = helpers.logging.warning;
-      break;
-
-    default:
-      console.log('error: incorrect logging level in config.json - should be warning or debug!');
-  }
-*/
-
-
-function handler(req, res) {
-
-  var question     = res.question[0], 
-      hostname     = question.name, 
-      length       = hostname.length, 
+const server = dnsd.createServer(function(req, res) {
+  var question     = res.question[0],
+      hostname     = question.name,
+      length       = hostname.length,
       ttl          = Math.floor(Math.random() * 3600),
       redis        = require("redis"),
       redis_client = redis.createClient(this.redisHost, this.redisPort);
-
 
   var answer = {};
 
@@ -67,9 +36,9 @@ function handler(req, res) {
         } else {
           if(redis_res !== null && redis_res.length > 0) {
             answer = {
-              name:hostname, 
-              type:'A', 
-              data:redis_res, 
+              name:hostname,
+              type:'A',
+              data:redis_res,
               'ttl':ttl};
 
             res.answer.push(answer);
@@ -141,9 +110,6 @@ function handler(req, res) {
     res.end();
   }
 
-}
-
-var server = dnsd.createServer(handler);
-console.log('Server running at'+this.dnsInterface+':'+this.dnsPort);
-server.zone(this.dnsZone, 'ns1.'+this.dnsZone, 'us@'+this.dnsZone, 'now', '2h', '30m', '2w', '10m')
-      .listen(this.dnsPort, this.dnsInterface);
+});
+console.log(`Server running at ${this.dnsInterface}:${this.dnsPort}`);
+server.zone(this.dnsZone, 'ns1.'+this.dnsZone, 'us@'+this.dnsZone, 'now', '2h', '30m', '2w', '10m').listen(this.dnsPort, this.dnsInterface);
