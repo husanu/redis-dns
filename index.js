e74f8dcd8c3cd2870c7ed0012b1cb4434e70838a
Alexis Tyler <xo@wvvw.me>
2017-04-05 17:41:58 +0930

    convert index.js to ES6, cleanup formatting

--- index.js
+++ index.js
@@ -1,115 +1,113 @@
 #!/usr/bin/env node
 
+import path from 'path';
+import util from 'util'
 import dnsd from 'dnsd';
 import nconf from 'nconf';
+import redis from 'redis';
+import dns from 'native-dns';;
 
 nconf.use('file', {
-    file: path.join(__dirname, 'redis-dns-config.json')
+    file: path.join(__dirname, 'config.json')
 });
 nconf.load();
 
-this.dnsInterface = nconf.get('dns_interface');
-this.dnsPort      = nconf.get('dns_port');
-this.dnsZone      = nconf.get('dns_zone');
-this.redisHost    = nconf.get('redis_host');
-this.redisPort    = nconf.get('redis_port');
+const config = {
+    dns: nconf.get('dns'),
+    redis: nconf.get('redis')
+};
 
 const server = dnsd.createServer(function(req, res) {
-  var question     = res.question[0],
-      hostname     = question.name,
-      length       = hostname.length,
-      ttl          = Math.floor(Math.random() * 3600),
-      redis        = require("redis"),
-      redis_client = redis.createClient(this.redisHost, this.redisPort);
+    const question = res.question[0];
+    const hostname = question.name;
+    const length = hostname.length;
+    const ttl = Math.floor(Math.random() * 3600);
+    const redisClient = redis.createClient(config.redis.port, config.redis.host);
 
-  var answer = {};
+    let answer = {};
 
-  if(question.type === 'A') {
+    if(question.type === 'A') {
+        redisClient.on('connect', function () {
 
-    // connect to redis
-    redis_client.on("connect", function () {
+            // first set the IP for the domain in redis
+            redisClient.get(`redis-dns:${hostname}`, function(redis_err, redis_res) {
+                if (redis_err) {
+                    console.log('Redis error:'+redis_err);
+                } else {
+                    if(redis_res !== null && redis_res.length > 0) {
+                        answer = {
+                            name: hostname,
+                            type: 'A',
+                            data: redis_res,
+                            ttl
+                        };
 
-      // first set the IP for the domain in redis
-      redis_client.get("redis-dns:"+hostname, function(redis_err, redis_res) {
-        if(redis_err) {
-          console.log('Redis error:'+redis_err);
-        } else {
-          if(redis_res !== null && redis_res.length > 0) {
-            answer = {
-              name:hostname,
-              type:'A',
-              data:redis_res,
-              'ttl':ttl};
+                        res.answer.push(answer);
 
-            res.answer.push(answer);
+                        redisClient.quit();
+                        console.info('%s:%s/%s question:%j answer:%j', req.connection.remoteAddress, req.connection.remotePort, req.connection.type, question, answer);
+                        res.end();
+                    } else {
 
-            redis_client.quit();
-            console.log('%s:%s/%s question:%j answer:%j', req.connection.remoteAddress, req.connection.remotePort, req.connection.type, question, answer);
-            res.end();
+                        /* No match in Redis, try to lookup using the google server */
+                        var nativeQuestion = dns.Question({
+                            name: question.name,
+                            type: 'A'
+                        });
 
-          } else {
+                        var start = Date.now();
 
-            /* No match in Redis, try to lookup using the google server */
+                        var nativeReq = dns.Request({
+                            question: nativeQuestion,
+                            server: {
+                                address: '8.8.8.8',
+                                port: 53,
+                                type: 'udp'
+                            },
+                            timeout: 1000
+                        });
 
-            var dns  = require('native-dns'),
-                util = require('util');
+                        nativeReq.on('timeout', function () {
+                            console.log('native-dns: Timeout in making request');
+                        });
 
-            var nativeQuestion = dns.Question({
-              name: question.name,
-              type: 'A'
-            });
+                        nativeReq.on('message', function (err, answer) {
+                            answer.answer.forEach(function(a) {
+                                answer = {
+                                    name: hostname,
+                                    type: 'A',
+                                    data: a.address,
+                                    ttl
+                                };
 
-            var start = Date.now();
+                                res.answer.push(answer);
+                            });
+                        });
 
-            var nativeReq = dns.Request({
-              question: nativeQuestion,
-              server: { address: '8.8.8.8', port: 53, type: 'udp' },
-              timeout: 1000
-            });
+                        nativeReq.on('end', function() {
+                            var delta = (Date.now()) - start;
+                            console.info(`Finished processing request: ${delta.toString()}ms`);
 
-            nativeReq.on('timeout', function () {
-              console.log('native-dns: Timeout in making request');
-            });
+                            redisClient.quit();
+                            console.info('%s:%s/%s question:%j answer:%j', req.connection.remoteAddress, req.connection.remotePort, req.connection.type, question, res.answer);
+                            res.end();
+                        });
 
-            nativeReq.on('message', function (err, answer) {
-              answer.answer.forEach(function (a) {
+                        nativeReq.send();
+                    }
+                }
+            }.bind(this));
+        }.bind(this));
 
-                answer = {
-                  name:hostname,
-                  type:'A',
-                  data:a.address,
-                  'ttl':ttl};
+        // redis error management
+        redisClient.on('error', function(err) {
+            console.error('Redis error', err);
+        });
 
-                res.answer.push(answer);
-              });
-            });
-
-            nativeReq.on('end', function () {
-              var delta = (Date.now()) - start;
-              console.log('Finished processing request: ' + delta.toString() + 'ms');
-
-              redis_client.quit();
-              console.log('%s:%s/%s question:%j answer:%j', req.connection.remoteAddress, req.connection.remotePort, req.connection.type, question, res.answer);
-              res.end();
-
-            });
-
-            nativeReq.send();
-
-          }
-        }
-      }.bind(this));
-    }.bind(this));
-
-    // redis error management
-    redis_client.on("error", function (err) {
-      console.log("Redis error: " + err);
-    });
-
-  } else {
-    res.end();
-  }
-
+    } else {
+        res.end();
+    }
 });
-console.log(`Server running at ${this.dnsInterface}:${this.dnsPort}`);
-server.zone(this.dnsZone, 'ns1.'+this.dnsZone, 'us@'+this.dnsZone, 'now', '2h', '30m', '2w', '10m').listen(this.dnsPort, this.dnsInterface);
+
+console.log(`Server running at ${config.dns.interface}:${config.dns.port}`);
+server.zone(config.dns.zone, 'ns1.' + config.dns.zone, 'us@' + config.dns.zone, 'now', '2h', '30m', '2w', '10m').listen(config.dns.port, config.dns.interface);
